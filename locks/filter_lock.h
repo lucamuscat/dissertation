6bf40129c9f267be987c2c01d4462957aea8cc15
Luca Muscat <luca.muscat.19@um.edu.mt>
2021-11-04 09:29:08 +0100

    Fix bug inside of filter_lock that was causing segfaults

--- locks/filter_lock.h
+++ locks/filter_lock.h
@@ -1,20 +1,22 @@
 #ifndef FILTER_LOCK_H
 #define FILTER_LOCK_H
 
+#include <stdbool.h>
 #include <stddef.h>
 
-typedef struct filter_lock
+typedef struct filter_lock_t
 {
     size_t* level;
     size_t* victim;
     size_t number_of_threads;
-} filter_lock;
+} filter_lock_t;
 
-int create_lock(size_t number_of_threads, filter_lock* lock);
+int create_lock(size_t number_of_threads, filter_lock_t** lock);
 
 // Lock mutex if not already locked
 // If already locked, wait
-void wait_lock(size_t thread_id, filter_lock* lock);
-void unlock(size_t thread_id, filter_lock* lock);
+void wait_lock(size_t thread_id, filter_lock_t* lock);
+void unlock(size_t thread_id, filter_lock_t* lock);
+bool stay_level(size_t index, size_t thread_id, filter_lock_t* lock);
 
 #endif

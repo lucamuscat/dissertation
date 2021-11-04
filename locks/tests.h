3da719db1cb3429d9e8b0be840f5e860f8a6f0c5
Luca Muscat <luca.muscat.19@um.edu.mt>
2021-11-04 09:36:46 +0100

    Create header file for potential tests that can be called

---
+++ locks/tests.h
@@ -0,0 +1,22 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+#include <stdbool.h>
+#include <stddef.h>
+
+/// A test that spawns multiple threads that
+/// will increment a single value inside of a shared
+/// struct.
+/// @param n: Number of threads.
+/// @returns: True if passed.
+/// False if failed.
+bool counter_test(size_t n);
+
+/// Tests that threads will spin lock
+/// when a mutex is already locked.
+/// @param n: Number of threads.
+/// @returns: True if passed.
+/// False if failed.
+bool spinlock_test(size_t n);
+
+#endif

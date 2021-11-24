0cecdd62dee06b847765b613e33d89e2826ceda9
Luca Muscat <luca.muscat.19@um.edu.mt>
2021-11-24 18:47:36 +0100

    Implement filter_lock.c using OMP

--- locks/filter_lock.c
+++ locks/filter_lock.c
@@ -1,105 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdatomic.h>
+#include <stdbool.h>
 #include <errno.h>
-#include "filter_lock.h"
+#include <omp.h>
+#include "lock.h"
+#include "globals.h"
 
-bool stay_level(size_t index, size_t thread_id, filter_lock_t* lock)
+#define P_LOCK ((filter_lock_t*)lock)
+
+typedef struct filter_lock_t
 {
-    for (size_t k = 0; k < lock->number_of_threads; ++k)
+    int* level;
+    int* victim;
+    size_t number_of_threads;
+} filter_lock_t;
+
+int create_lock(void** lock)
+{
+    const int num_of_threads = omp_get_num_threads();
+    filter_lock_t** temp = (filter_lock_t**)lock;
+    *temp = (filter_lock_t*)malloc(sizeof(filter_lock_t));
+    if (temp != NULL)
     {
-        // There exists k != me
-        if (k == thread_id)
-            continue;
-        if (lock->level[k] >= index && lock->victim[index] == thread_id)
-            return true;
-    }
-    return false;
-}
-
-bool stay_level_stm(size_t index, size_t thread_id, filter_lock_t* lock)
-{
-    __transaction_atomic{
-        for (size_t k = 0; k < lock->number_of_threads; ++k)
-        {
-            // There exists k != me
-            if (k == thread_id)
-                continue;
-            if (lock->level[k] >= index && lock->victim[index] == thread_id)
-                return true;
-        }
-    }
-    return false;
-}
-
-int create_lock(size_t number_of_threads, filter_lock_t** lock)
-{
-    *lock = (filter_lock_t*)malloc(sizeof(filter_lock_t));
-    if (lock != NULL)
-    {
-        (*lock)->level = (size_t*)malloc(sizeof(size_t)*number_of_threads);
-        if ((*lock)->level == NULL)
+        (*temp)->level = (int*)malloc(sizeof(int) * num_of_threads);
+        if ((*temp)->level == NULL)
             return errno;
-        (*lock)->victim = (size_t*)malloc(sizeof(size_t) * number_of_threads);
-        if ((*lock)->victim == NULL)
+        (*temp)->victim = (int*)malloc(sizeof(int) * num_of_threads);
+        if ((*temp)->victim == NULL)
             return errno;
-        for (size_t i = 0; i < number_of_threads; ++i)
+        for (int i = 0; i < num_of_threads; ++i)
         {
-            (*lock)->level[i] = 0;
-            (*lock)->victim[i] = 0;
+            (*temp)->level[i] = 0;
+            (*temp)->victim[i] = 0;
         }
         return 0;
     }
     return errno;
 }
 
-void free_lock(filter_lock_t* lock)
+void free_lock(void* lock)
 {
-    free(lock->level);
-    free(lock->victim);
-    free(lock);
+    //free(P_LOCK->level);
+    //free(P_LOCK->victim);
+    //free(P_LOCK);
 }
 
-// TODO: Solve race conditions using the following concepts
-// [] Atomics
-// [] STM (Software-based Transactional Memory)
-// [] Memory barriers
-void wait_lock(size_t thread_id, filter_lock_t* lock)
+void wait_lock(void* lock)
 {
-    for (size_t i = 1; i < lock->number_of_threads; ++i)
+    int num_of_threads = omp_get_num_threads();
+    int thread_id = omp_get_thread_num();
+    for (int i = 1; i < num_of_threads; ++i)
     {
-        // Can race conditions in this section be fixed through memory barriers?
-        lock->level[thread_id] = i;
-        lock->victim[i] = thread_id;
+        P_LOCK->level[thread_id] = i;
+        P_LOCK->victim[i] = thread_id;
         // Spin lock
-        while (stay_level(i, thread_id, lock));
-    }
-}
-
-// wait_lock using software-based transactional memory
-void wait_lock_stm(size_t thread_id, filter_lock_t* lock)
-{
-    for (size_t i = 1; i < lock->number_of_threads; ++i)
-    {
-        __transaction_atomic{
-            lock->level[thread_id] = i;
-            lock->victim[i] = thread_id;
+        bool stay = true;
+        while (stay)
+        {
+            stay = false;
+            for (int k = 0; k < num_of_threads; ++k)
+            {
+                if (k == thread_id)
+                    continue;
+                if (P_LOCK->level[k] >= i && P_LOCK->victim[i] == thread_id)
+                {
+                    stay = true;
+                    break;
+                }
+            }
         }
-        while (stay_level_stm(i, thread_id, lock));
-        // Spin lock
     }
 }
 
-void unlock_stm(size_t thread_id, filter_lock_t* lock)
+void unlock(void* lock)
 {
-    __transaction_atomic{
-        lock->level[thread_id] = 0;
-    }
+    P_LOCK->level[omp_get_thread_num()] = 0;
 }
-
-
-void unlock(size_t thread_id, filter_lock_t* lock)
-{
-    lock->level[thread_id] = 0;
-}
-

a00c21716432fe76c5a2777b42520b49620550ce
Luca Muscat <luca.muscat.19@um.edu.mt>
2022-02-25 12:29:40 +0100

    Remove diff from test_utils

--- locks/tests/test_utils.c
+++ locks/tests/test_utils.c
@@ -16,20 +16,3 @@ int handle_args(int argc, char* argv[])
     omp_set_num_threads(number_of_threads);
     return number_of_threads;
 }
-
-// Adapted from http://www.guyrutenberg.com/2007/09/22/profiling-code-using-clock_gettime/
-int64_t diff(timespec* start,timespec* end)
-{
-    timespec temp;
-    if ((end->tv_nsec - start->tv_nsec) < 0)
-    {
-        temp.tv_sec = end->tv_sec - start->tv_sec - 1;
-        temp.tv_nsec = 1000000000 + end->tv_nsec - start->tv_nsec;
-    }
-    else
-    {
-        temp.tv_sec = end->tv_sec - start->tv_sec;
-        temp.tv_nsec = end->tv_nsec - start->tv_nsec;
-    }
-    return temp.tv_nsec;
-}
--- locks/tests/test_utils.h
+++ locks/tests/test_utils.h
@@ -5,7 +5,6 @@
 
 typedef struct timespec timespec;
 
-int64_t diff(timespec* start, timespec* end);
 int handle_args(int argc, char* argv[]);
 
 #endif

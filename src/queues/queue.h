0b8b390fe5a685fda7f234405aef7302977bf74d
Luca Muscat <luca.muscat.19@um.edu.mt>
2022-02-25 13:59:21 +0100

    Add comments to header files

--- src/queues/queue.h
+++ src/queues/queue.h
@@ -1,8 +1,31 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+/**
+ * @brief Enqueue an item inside the queue.
+ * 
+ * @param queue Queue in which the item will be enqueued.
+ * @param in_item Item which will be enqueued inside of the queue.
+ * @return Status
+ */
 int enqueue(void* queue, void* in_item);
+
+/**
+ * @brief Dequeue an item from the queue.
+ * 
+ * @param queue Queue in which an item will be dequeued
+ * @param out_item Item in which the dequeued item will be stored.
+ * @return Status
+ */
 int dequeue(void* queue, void* out_item);
+
+/**
+ * @brief Initializes a queue.
+ * 
+ * @param out_queue Pointer to the queue that is to be initialized
+ * @return Status
+ */
+int create_queue(void** out_queue);
 
 // A randomly poke method might come in handy in the future:
 // https://youtu.be/_qaKkHuHYE0?t=1073
--- src/test_utils.h
+++ src/test_utils.h
@@ -5,6 +5,14 @@
 
 typedef struct timespec timespec;
 
+/**
+
+ * @brief Processes the number of threads provided in the command line argument.
+ * 
+ * @param argc Number of arguments provided by the user
+ * @param argv Array of arguments provided by the user
+ * @return Number of threads specified in the command line arguments.
+ */
 int handle_args(int argc, char* argv[]);
 
 #endif

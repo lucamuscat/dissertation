d09111e9487cd3ad74f03e00b9aa0c82f2dc52b3
Luca Muscat <lucamuscat@gmail.com>
2022-03-02 15:17:47 +0100

    Increase number of test iterations in enqueue_dequeue

--- src/queues/tests/enqueue_dequeue.c
+++ src/queues/tests/enqueue_dequeue.c
@@ -5,7 +5,9 @@
 #include "../../test_utils.h"
 #include "../queue.h"
 
-#define TOTAL_ENQUEUE_DEQUEUE_PAIRS_PER_THREAD 100000
+#define TOTAL_ENQUEUE_DEQUEUE_PAIRS_PER_THREAD 1000000
+#define TOTAL_RERUNS 100000
+#define NANO_TO_SEC 1000000000
 
 int main(int argc, char** argv)
 {
@@ -15,30 +17,48 @@ int main(int argc, char** argv)
         return num_of_threads;
     
     void* queue;
+
     
     PASS((create_queue(&queue)));
-    long long diff;
-    long long total_diff = 0;
-
-    #pragma omp parallel shared(queue, total_diff) private(diff)
+    long long total_cycle_diff = 0;
+    long long total_ns_diff = 0;
+    long long total_run_time_ns = PAPI_get_real_nsec();
+    
+    #pragma omp parallel shared(queue, total_cycle_diff, total_ns_diff)
     {
+        long long cycle_diff;
+        long long ns_diff;
         void* dequeued_item = NULL;
-        diff = PAPI_get_real_cyc();
-        for (size_t i = 0; i < TOTAL_ENQUEUE_DEQUEUE_PAIRS_PER_THREAD; ++i)
+        long long inner_total_diff = 0LL;
+        long long inner_total_ns_diff = 0LL;
+        for (size_t j = 0; j < TOTAL_RERUNS; ++j)
         {
-            enqueue(queue, &diff);
-            dequeue(queue, &dequeued_item);
+            cycle_diff = PAPI_get_real_cyc();
+            ns_diff = PAPI_get_real_nsec();
+            for (size_t i = 0; i < TOTAL_ENQUEUE_DEQUEUE_PAIRS_PER_THREAD; ++i)
+            {
+                enqueue(queue, &cycle_diff);
+                dequeue(queue, &dequeued_item);
+            }
+            cycle_diff = PAPI_get_real_cyc() - cycle_diff;
+            ns_diff = PAPI_get_real_nsec() - ns_diff;
+            inner_total_diff += cycle_diff / TOTAL_ENQUEUE_DEQUEUE_PAIRS_PER_THREAD;
+            inner_total_ns_diff += ns_diff / TOTAL_ENQUEUE_DEQUEUE_PAIRS_PER_THREAD;
         }
-        diff = PAPI_get_real_cyc() - diff;
         #pragma omp critical
-        total_diff += diff;
+        {
+            total_cycle_diff += inner_total_diff / TOTAL_RERUNS;
+            total_ns_diff += inner_total_ns_diff / TOTAL_RERUNS;
+        }
+
     }
 
-    long long average_per_thread = total_diff / num_of_threads;
-    long long average_per_iteration = average_per_thread / TOTAL_ENQUEUE_DEQUEUE_PAIRS_PER_THREAD;
+    total_run_time_ns = total_run_time_ns - PAPI_get_real_nsec();
     
-    printf("Average Per Thread: %lld cycles\n", average_per_thread);
-    printf("Average Iteration: %lld cycles\n", average_per_iteration);
+    long long average_cycles = total_cycle_diff / num_of_threads;
+
+    printf("Total runtime: %lld", total_run_time_ns / NANO_TO_SEC);
+    printf("Average Per Thread: %lld cycles\n", average_cycles);
 
     return 0;
 }

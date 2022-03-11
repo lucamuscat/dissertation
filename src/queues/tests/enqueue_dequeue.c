20e444573b0157c7c41e1578783e778f8983087d
Luca Muscat <lucamuscat@gmail.com>
2022-03-11 12:34:51 +0100

    Gather the mean and the standard deviation of each batch of readings in enqueue_dequeue

--- src/queues/tests/enqueue_dequeue.c
+++ src/queues/tests/enqueue_dequeue.c
@@ -13,11 +13,12 @@
 #include <math.h>
 #include <pthread.h>
 #include <assert.h>
+#include <stdatomic.h>
 #include "../../test_utils.h"
 #include "../queue.h"
 
 #define TEST_ITERATIONS 100000
-#define TEST_RERUNS 1
+#define TEST_RERUNS 5
 #define NANO_TO_MINUTE 1000000000*60
 #define RANDOM_RANGE 5
 
@@ -26,40 +27,48 @@ static int* random_delays_ns;
 
 typedef struct thread_args
 {
+    pthread_mutex_t* mutex;
     void* queue;
     size_t num_of_iterations;
-    double* total_cycle_diff;
-    double* total_ns_diff;
-    pthread_mutex_t* mutex;
     pthread_t tid;
-} __attribute__((aligned(CACHE_LINE_SIZE))) thread_args;
+    double* cycles_readings;
+    double* nsec_readings;
+} CACHE_ALIGNED /* Align to cache to avoid false sharing */ thread_args;
 
+/*
+ * TODO:
+    1. Place the mean cycle and nanosecond time inside of an array (instead of
+    aggregating it);
+
+    2. Calculate the standard deviation of all of the means from all of the
+    threads
+
+    3. Measure misaligned memory access counters.
+*/
 void* thread_fn(void* in_args)
 {
     thread_args* args = in_args;
     long long cycle_diff;
     long long ns_diff;
     void* dequeued_item = NULL;
-    double inner_total_diff = 0.0;
-    double inner_total_ns_diff = 0.0;
+    int enqueued_item = 0;
     
-    for (size_t j = 0; j < TEST_RERUNS; ++j)
+    for (size_t i = 0; i < TEST_RERUNS; ++i)
     {
         // Make sure that each thread executes the test at the same time.
         pthread_barrier_wait(&barrier);
-        //TODO: Use PAPI_TOT_CYC instead of PAPI_REF_CYC
         cycle_diff = PAPI_get_real_cyc();
         ns_diff = PAPI_get_real_nsec();
-        for (size_t i = 0; i < args->num_of_iterations; ++i)
+        for (size_t j = 0; j < args->num_of_iterations; ++j)
         {
-            enqueue(args->queue, &cycle_diff);
-            DELAY(random_delays_ns[i]);
+            enqueue(args->queue, &enqueued_item);
+            DELAY(random_delays_ns[j]);
             dequeue(args->queue, &dequeued_item);
-            DELAY(random_delays_ns[i]);
+            DELAY(random_delays_ns[j]);
         }
         // FIXME: Remove delay from ns_diff and cycle_diff
-        cycle_diff = PAPI_get_real_cyc() - cycle_diff;
-        ns_diff = PAPI_get_real_nsec() - ns_diff;
+        args->cycles_readings[i] = (PAPI_get_real_cyc() - cycle_diff) / args->num_of_iterations;
+        args->nsec_readings[i] = (PAPI_get_real_nsec() - ns_diff) / args->num_of_iterations;
 
         // We're working with concurrent queues.
         // So might aswell concurrently empty it.
@@ -85,49 +94,61 @@ int main(int argc, char** argv)
     pthread_mutex_t mutex;
     PASS_LOG(pthread_mutex_init(&mutex, NULL) == 0, "Failed to create pthread_mutex");
 
-    double total_cycle_diff = 0, total_ns_diff = 0;
-
-    // Ensure that exactly TEST_ITERATIONS iterations occur, as integer division
-    // might cause truncation and either increase or reduce the total number of
-    // iterations.
-    const int iterations_per_thread = (int)floor(((double)TEST_ITERATIONS) / num_of_threads);
-    const int final_thread_iterations = TEST_ITERATIONS - (iterations_per_thread * (num_of_threads - 1));
-
     random_delays_ns = generate_random_numbers(TEST_ITERATIONS, delay_ns - RANDOM_RANGE, delay_ns + RANDOM_RANGE);
 
-    assert((iterations_per_thread * (num_of_threads - 1) + final_thread_iterations) == TEST_ITERATIONS);
     assert(sizeof(thread_args) == CACHE_LINE_SIZE);
 
     PASS_LOG(pthread_barrier_init(&barrier, NULL, num_of_threads) == 0, "Failed to create pthread_barrier");
 
     thread_args* args = (thread_args*)malloc(sizeof(thread_args) * num_of_threads);
-    size_t total_iterations = 0;
+    P_PASS(args);
+
+    size_t accumulated_iterations = 0;
     long long total_run_time_ns = PAPI_get_real_nsec();
-    for (int i = 0; i < num_of_threads; ++i)
+
+    for (size_t i = 0; i < num_of_threads; ++i)
     {
         args[i].mutex = &mutex;
         args[i].queue = queue;
-        args[i].total_cycle_diff = &total_cycle_diff;
-        args[i].total_ns_diff = &total_ns_diff;
-        args[i].num_of_iterations = i < num_of_threads - 1 ? iterations_per_thread : final_thread_iterations;
-        total_iterations += args[i].num_of_iterations;
+
+        args[i].cycles_readings = (double*)malloc(sizeof(double) * TEST_RERUNS);
+        args[i].nsec_readings = (double*)malloc(sizeof(double) * TEST_RERUNS);
+        P_PASS(args[i].cycles_readings);
+        P_PASS(args[i].nsec_readings);
+        
+        args[i].num_of_iterations = iterations_per_thread(num_of_threads, i, TEST_ITERATIONS);
+        accumulated_iterations += args[i].num_of_iterations;
         pthread_create(&args[i].tid, NULL, thread_fn, &args[i]);
     }
 
     // Sanity check; Ensure that the total iterations match up.
-    assert(total_iterations == TEST_ITERATIONS);
+    assert(accumulated_iterations == TEST_ITERATIONS);
     
-    for (int i = 0; i < num_of_threads; ++i)
+    for (size_t i = 0; i < num_of_threads; ++i)
     {
         pthread_join(args[i].tid, NULL);
     }
 
     total_run_time_ns = PAPI_get_real_nsec() - total_run_time_ns;
+
+    double** cycles = (double**)malloc(sizeof(double*) * num_of_threads);
+    double** nanoseconds = (double**)malloc(sizeof(double*) * num_of_threads);
+
+    for (size_t i = 0; i < num_of_threads; ++i)
+    {
+        cycles[i] = args[i].cycles_readings;
+        nanoseconds[i] = args[i].nsec_readings;
+    }
     
-    double average_cycles = total_cycle_diff / num_of_threads;
-    double average_ns = total_ns_diff / num_of_threads;
-    
-    printf("\"%s\", %d, %f, %f, %d, %lld\n", get_queue_name(), num_of_threads, average_cycles, average_ns, delay_ns, total_run_time_ns);
+    double average_cycles = mean_2d(cycles, num_of_threads, TEST_RERUNS);
+    double stdev_cycles = stdev_2d(cycles, num_of_threads, TEST_RERUNS);
+    double average_nsecs = mean_2d(nanoseconds, num_of_threads, TEST_RERUNS);
+    double stdev_nsecs = stdev_2d(nanoseconds, num_of_threads, TEST_RERUNS);
+
+    printf("\"%s\", %zu, %zu, ", get_queue_name(), num_of_threads, delay_ns);
+    printf("%f, %f, %f, %f, ", average_cycles, average_nsecs, stdev_cycles, stdev_nsecs);
+    printf("%f, %f, ", stdev_cycles / average_cycles, stdev_nsecs / average_nsecs);
+    printf("%lld\n", total_run_time_ns);
 
     return 0;
 }

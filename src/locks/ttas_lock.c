713c5ad19a3d0c192f6fbf110a1322722410986f
Luca Muscat <lucamuscat@gmail.com>
2022-03-24 15:15:16 +0100

    Update ttas_lock to work with clang
    * Fixed cases where atomic_bool was being used as an atomic_flag; causing errors to be thrown
    * Add _mm_pause() inside the test part of the lock to reduce contention (as suggested by intels xeon locking analysis paper)

--- src/locks/ttas_lock.c
+++ src/locks/ttas_lock.c
@@ -1,6 +1,7 @@
 #include <stdatomic.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <immintrin.h>
 #include "lock.h"
 #include "globals.h"
 
@@ -14,10 +15,11 @@ typedef struct ttas_lock_t
 
 bool create_lock(void** lock)
 {
-    *lock = malloc(sizeof(ttas_lock_t));
-    if (*lock == NULL)
+    ttas_lock_t** temp = (ttas_lock_t**)lock;
+    *temp = (ttas_lock_t*)malloc(sizeof(ttas_lock_t));
+    if (*temp == NULL)
         return false;
-    atomic_init(&P_LOCK->busy, 0);
+    atomic_init(&(*temp)->busy, 0);
     return true;
 }
 
@@ -26,17 +28,24 @@ void destroy_lock(void** lock)
     free(*lock);
 }
 
+// Source: https://stackoverflow.com/a/33048193
 void wait_lock(void* lock)
 {
     do
     {
-        while (atomic_load_explicit(&(P_LOCK->busy), memory_order_acquire));
-    } while (atomic_exchange_explicit(&(P_LOCK->busy), 1, memory_order_acquire));
+        // Spin whilst busy == 1
+        while (atomic_load_explicit(&P_LOCK->busy, memory_order_acquire))
+            _mm_pause();
+        // Set busy to 1, if busy is already 1, loop again.
+        // The method loops again on the condition at least two threads
+        // see the busy flag being set to zero at the same time.
+    } while (atomic_exchange_explicit(&P_LOCK->busy, 1, memory_order_acquire));
 }
 
 void unlock(void* lock)
 {
-    atomic_flag_clear_explicit(&(P_LOCK->busy), memory_order_release);
+    ttas_lock_t* temp = (ttas_lock_t*)lock;
+    atomic_store_explicit(&temp->busy, 0, memory_order_release);
 }
 
 char* get_lock_name()

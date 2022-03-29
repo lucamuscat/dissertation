67ccb21f1ba556ae8d51edcf6b5cb5b34525b435
Luca Muscat <lucamuscat@gmail.com>
2022-03-29 19:27:04 +0200

    Refactor p_enqueue_dequeue to be similar to enqueue_dequeue

--- src/queues/tests/p_enqueue_dequeue.c
+++ src/queues/tests/p_enqueue_dequeue.c
@@ -20,9 +20,6 @@
 #include "../../test_utils.h"
 #include "../queue.h"
 
-#define TEST_ITERATIONS 100000
-#define TEST_RERUNS 5
-
 delay_t delay;
 pthread_barrier_t barrier;
 
@@ -38,35 +35,35 @@ typedef struct thread_args
 
 void* thread_fn(void* in_args)
 {
-    PASS_LOG(PAPI_register_thread() == PAPI_OK, "Failed to register thread");
+    // PASS_LOG(PAPI_register_thread() == PAPI_OK, "Failed to register thread");
     thread_args* args = in_args;
+    register_thread(args->iterations);
     int enqueued_item = 10;
-    void* dequeued_item = 0;
-    for (size_t i = 0; i < TEST_RERUNS; ++i)
+    void* dequeued_item = NULL;
+
+    pthread_barrier_wait(&barrier);
+    start_readings(args->readings);
+    
+    for (int i = 0; i < args->iterations; ++i)
     {
-        pthread_barrier_wait(&barrier);
-        start_readings(args->readings);
-        for (int j = 0; j < args->iterations; ++j)
-        {
-            if (args->random_probabilities[j] < args->p)
-                enqueue(args->queue, (void**)&enqueued_item);
-            else
-                dequeue(args->queue, (void*)&dequeued_item);
-            DELAY_OPS(delay.num_of_nops);
-        }
-        delta_readings(args->readings, args->iterations);
-        adjust_readings_for_delay(args->readings, &delay);
-        // Empty the queue before the next test run.
-        while (dequeue(args->queue, (void**)&dequeued_item))
+        if (args->random_probabilities[i] < args->p)
+            enqueue(args->queue, (void**)&enqueued_item);
+        else if (dequeue(args->queue, (void*)&dequeued_item))
             assert(*((int*)dequeued_item) == enqueued_item);
-        
+
+        DELAY_OPS(delay.num_of_nops);
     }
-    PASS_LOG(PAPI_unregister_thread() == PAPI_OK, "Failed to unregister thread");
+    
+    delta_readings(args->readings, args->iterations);
+    adjust_readings_for_delay(args->readings, &delay);
+    cleanup_thread();
+    
+    // PASS_LOG(PAPI_unregister_thread() == PAPI_OK, "Failed to unregister thread");
     atomic_thread_fence(memory_order_seq_cst);
     return NULL;
 }
 
-int main(int argc, char const* argv[])
+void p_handle_args(int argc, char** argv, size_t* num_of_threads, size_t* delay_ns, double* p)
 {
     if (argc != 4)
     {
@@ -74,22 +71,30 @@ int main(int argc, char const* argv[])
         fprintf(stderr, "\targ1: uint - Number of threads\n");
         fprintf(stderr, "\targ2: uint - Reentrancy delay in nanoseconds\n");
         fprintf(stderr, "\targ3: double - Probability of enqueue (p)\n");
-        return EXIT_FAILURE;
+        exit(EXIT_FAILURE);
     }
 
+    char* pEnd[3] = { "" };
+    *num_of_threads = strtoul(argv[1], &pEnd[0], 10);
+    *delay_ns = strtoul(argv[2], &pEnd[1], 10);
+    *p = strtod(argv[3], &pEnd[2]);
+}
+
+int main(int argc, char** argv)
+{
+    /*
     int retval;
     if ((retval = PAPI_library_init(PAPI_VER_CURRENT)) != PAPI_VER_CURRENT)
     {
         PAPI_perror("");
         return EXIT_FAILURE;
     }
+    */
 
-    PASS_LOG(PAPI_thread_init((unsigned long (*)(void)) (pthread_self)) == PAPI_OK, "PAPI_thread_init failed");
-
-    char* pEnd[3] = { "" };
-    size_t num_of_threads = strtoul(argv[1], &pEnd[0], 10);
-    size_t delay_ns = strtoul(argv[2], &pEnd[1], 10);
-    double p = strtod(argv[3], &pEnd[2]);
+    // PASS_LOG(PAPI_thread_init((unsigned long (*)(void)) (pthread_self)) == PAPI_OK, "PAPI_thread_init failed");
+    size_t num_of_threads, delay_ns;
+    double p;
+    p_handle_args(argc, argv, &num_of_threads, &delay_ns, &p);
 
     calibrate_delay(&delay, delay_ns);
     
@@ -97,44 +102,52 @@ int main(int argc, char const* argv[])
     assert(p <= 1);
 
     double** random_probabilities = (double**)malloc(sizeof(double*) * num_of_threads);
+    ASSERT_NOT_NULL(random_probabilities);
     thread_args* args = (thread_args*)malloc(sizeof(thread_args) * num_of_threads);
+    ASSERT_NOT_NULL(args);
 
-    void* queue;
-    PASS_LOG(create_queue(&queue), "Failed to create queue");
+    readings_t** readings = (readings_t**)malloc(sizeof(readings_t*) * num_of_threads);
+    ASSERT_NOT_NULL(readings);
+
+    for (size_t i = 0; i < num_of_threads; ++i)
+    {
+        create_readings(&readings[i], TEST_RERUNS);
+    }
+
     PASS_LOG(pthread_barrier_init(&barrier, NULL, num_of_threads) == 0, "Failed to create barrier");
 
-    for (size_t i = 0; i < num_of_threads; ++i)
+    for (size_t i = 0; i < TEST_RERUNS; ++i)
     {
-        int thread_iterations = iterations_per_thread(num_of_threads, i, TEST_ITERATIONS);
-        random_probabilities[i] = (double*)malloc(sizeof(double) * thread_iterations);
-        for (int j = 0; j < thread_iterations; ++j)
+        void* queue;
+        PASS_LOG(create_queue(&queue), "Failed to create queue");
+        for (size_t j = 0; j < num_of_threads; ++j)
         {
-            random_probabilities[i][j] = drand48();
+            int thread_iterations = iterations_per_thread(num_of_threads, j, TEST_ITERATIONS);
+            random_probabilities[j] = (double*)malloc(sizeof(double) * thread_iterations);
+            ASSERT_NOT_NULL(random_probabilities[j]);
+            for (int k = 0; k < thread_iterations; ++k)
+            {
+                random_probabilities[j][k] = drand48();
+            }
+            args[j].readings = readings[j];
+            args[j].iterations = thread_iterations;
+            args[j].queue = queue;
+            args[j].p = p;
+            args[j].random_probabilities = random_probabilities[j];
+            pthread_create(&args[j].tid, NULL, thread_fn, (void*)&args[j]);
         }
-        create_readings(&args[i].readings, TEST_RERUNS);
-        args[i].iterations = thread_iterations;
-        args[i].queue = queue;
-        args[i].p = p;
-        args[i].random_probabilities = random_probabilities[i];
-        pthread_create(&args[i].tid, NULL, thread_fn, (void*)&args[i]);
+
+        for (size_t j = 0; j < num_of_threads; ++j)
+        {
+            pthread_join(args[j].tid, NULL);
+        }
+        destroy_queue(&queue);
     }
 
-    for (size_t i = 0; i < num_of_threads; ++i)
-    {
-        pthread_join(args[i].tid, NULL);
-    }
-
-    readings_t** temp = (readings_t**)malloc(sizeof(readings_t*) * num_of_threads);
-
-    for (size_t i = 0; i < num_of_threads; ++i)
-    {
-        temp[i] = args[i].readings;
-    }
-
-    readings_t* readings = aggregate_readings_2d(temp, num_of_threads, TEST_RERUNS);
+    readings_t* aggregates = aggregate_readings_2d(readings, num_of_threads, TEST_RERUNS);
 
     printf("\"%s\", %ld, %zu, ", get_queue_name(), num_of_threads, delay_ns);
-    display_readings(readings);
+    display_readings(aggregates);
     puts("");
 
     return EXIT_SUCCESS;

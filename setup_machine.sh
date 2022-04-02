531391bd80ac1a44b072d005a1b9b090d1307a6c
Luca Muscat <lucamuscat@gmail.com>
2022-04-02 16:58:38 +0200

    Fix setup_machine.sh to work without root

--- setup_machine.sh
+++ setup_machine.sh
@@ -2,7 +2,7 @@
 
 # Switch off hyper-threading
 # https://askubuntu.com/a/1168248
-# sudo echo off > /sys/devices/system/cpu/smt/control
+echo off | sudo tee /sys/devices/system/cpu/smt/control
 
 # Disable CPU Frequency scaling
 # https://askubuntu.com/a/580785
@@ -10,4 +10,4 @@ sudo cpufreq-set -g performance
 
 # Disable intel turbo
 # https://askubuntu.com/a/620114
-sudo echo "1" | tee /sys/devices/system/cpu/intel_pstate/no_turbo
+echo "1" | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo

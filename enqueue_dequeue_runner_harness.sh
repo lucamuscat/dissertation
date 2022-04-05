bd32e2cbaf9388193224772129a082cdf0603e56
Luca Muscat <lucamuscat@gmail.com>
2022-04-05 20:16:41 +0200

    Update the test runner harness

--- enqueue_dequeue_runner_harness.sh
+++ enqueue_dequeue_runner_harness.sh
@@ -11,12 +11,12 @@ mkdir -p $dir_path
 file_name=$dir_path/$current_epoch.csv
 
 echo "name,threads,delay,time_ns,net_runtime_s,stdev_ns,p_ns,total_runtime_ns" > $file_name
-echo "$current_epoch: $2" | tee -a src/utils/readings/index.txt
+echo "$current_epoch: $2" | tee -a $dir_path/index.txt
 
-for num_of_threads in $(seq 4)
+for num_of_threads in $(seq 8)
 do
     for reruns in $(seq 5)
     do
-    ./build/nonblocking_$1 $num_of_threads 100 | tee -a $file_name
+    ./build/$1 $num_of_threads 100 | tee -a $file_name
     done
 done

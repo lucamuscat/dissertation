7125dc97ba4a5727c04b503afdc22cf4a1b767f1
Luca Muscat <lucamuscat@gmail.com>
2022-04-06 20:59:27 +0200

    Remove inline specifiers to prevent compilation errors

--- src/queues/non-blocking/ms_queue.c
+++ src/queues/non-blocking/ms_queue.c
@@ -75,12 +75,12 @@ void cleanup_thread()
  * called the register_thread(1) method.
  * @param out_node
  */
-inline void create_node(node_t** out_node)
+void create_node(node_t** out_node)
 {
     *out_node = &node_pool[++node_count];
 }
 
-inline void create_sentinel_node()
+void create_sentinel_node()
 {
     sentinel = (node_t*)calloc(1, sizeof(node_t));
     tagged_ptr_t null_node = pack_ptr(NULL, 0);

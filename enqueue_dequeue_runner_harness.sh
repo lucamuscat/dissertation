6f8cb008bf7796c4ba495f329801da595399c83d
Luca Muscat <lucamuscat@gmail.com>
2022-04-28 12:53:04 +0200

    Update enqueue_dequeue_runner_harness.sh
    * Add the p_enqueue_dequeue tests into the harness;
    * Rerun for three times instead of five;
    * Sweep up to 12 threads instead of eight.
    * Change delays to [0, 250, 750, 1000]
    * Output results in the root dir instead of src/utils/readings

--- enqueue_dequeue_runner_harness.sh
+++ enqueue_dequeue_runner_harness.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
-# $1 - Name of enqueue_dequeue benchmark binary to run
-# $2 - Comment to leave in the index
+reruns=3
+up_to_threads=12
+delays=(0 250 500 750 1000)
 
-current_epoch=$EPOCHSECONDS
+# $1 - Name of enqueue_dequeue benchmark binary to run
+
+# current_epoch=$EPOCHSECONDS
 dir_path=src/utils/readings/$1
 
 mkdir -p $dir_path
 
-file_name=$dir_path/$current_epoch.csv
+file_name=enqueue_dequeue_results.csv
+p_file_name=p_enqueue_dequeue_results.csv
 
 echo "name,threads,delay,time_ns,net_runtime_s,stdev_ns,p_ns,total_runtime_ns" > $file_name
-echo "$current_epoch: $2" | tee -a $dir_path/index.txt
+echo "name,threads,delay,time_ns,net_runtime_s,stdev_ns,p_ns,total_runtime_ns" > $p_file_name
 
-for num_of_threads in $(seq 8)
+# echo "$current_epoch: $2" | tee -a $dir_path/index.txt
+
+for num_of_threads in $(seq $up_to_threads)
 do
-    for reruns in $(seq 5)
+    for delay in ${delays[@]}
     do
-    ./build/$1 $num_of_threads 100 | tee -a $file_name
+        for reruns in $(seq $reruns)
+        do
+            ./build/$1 $num_of_threads $delay | tee -a $file_name
+            ./build/p_$1 $num_of_threads $delay 0.5 | tee -a $p_file_name
+        done
     done
 done

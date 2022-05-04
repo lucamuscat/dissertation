3a1e42b83bdc0b82e3a9248399088e40a6417ba0
Luca Muscat <luca.muscat.19@um.edu.mt>
2022-05-04 18:09:09 +0200

    Refactor plot.py to generate more consistent looking graphs
    * Graphs were made more consistent looking by using the same colour palette; for some reason, this wasn't the case.

--- src/utils/plot.py
+++ src/utils/plot.py
@@ -20,46 +20,46 @@ sns_kwargs = {
     "err_style": "bars",
     "linewidth": linewidth,
     "markersize": markersize,
-    "err_kws": {"linewidth" : linewidth, "capsize" : 2, "ecolor" : "k"}
+    "err_kws": {"linewidth" : linewidth, "capsize" : 2, "ecolor" : "k"},
 }
 
+labels_kwargs = {"xlabel": "Threads", "ylabel": "Net Runtime (s)"}
 
+sns.set_style("whitegrid")
+sns.set_context("paper")
 
-def plot_cleanup():
-    # Disable matplotlib's colour cycling after each plot
-    # ensuring idempotent results
-    #plt.gca().set_prop_cycle(None)
-    plt.close()
+def save_plot(ax: plt.Axes, output_file_name:str, plot_title:str, dpi:int):
+    ax.set(
+        title = plot_title,
+        **labels_kwargs
+    )
+    ax.get_figure().savefig(output_file_name, dpi = dpi)
+    ax.get_figure().clf()
+    
+
 
 def plot_grouped_results(delay:int, input_file_name:str, output_file_name:str, plot_title:str, dpi:int = 300):
     df = pd.read_csv(f"{input_file_name}.csv")
     individual_delay_df = df.query(f"delay == {str(delay)}").sort_values(by="name")
+    palette = sns.color_palette(n_colors=len(df["name"].unique()))
 
-    sns.set_style("whitegrid")
-    sns.set_palette("colorblind")
-
-    ax1 = sns.lineplot(
+    ax = sns.lineplot(
         data = individual_delay_df,
         x = "threads",
         y = "net_runtime_s",
         style = "name",
         hue = "name",
+        palette = palette,
         **sns_kwargs
     )
 
-    ax1.set_xlabel("Threads")
-    ax1.set_ylabel("Runtime (s)")
-    ax1.set_title(plot_title)
-
-    plt.savefig(f"{images_path}/{output_file_name}.jpg", dpi = dpi)
-    plot_cleanup()
+    save_plot(ax, f"{images_path}/{output_file_name}.jpg", plot_title, dpi)
 
 def plot_individual_results(input_file_name:str, output_file_name:str, plot_title:str, dpi:int = 300):
     df = pd.read_csv(f"{input_file_name}.csv", index_col="name")
     queue_names = df.index.unique().sort_values()
+    palette = sns.color_palette(n_colors=len(df["delay"].unique()))
 
-    sns.set_style("whitegrid")
-    sns.set_palette("colorblind")
 
     for queue_name in queue_names:
         individual_df = df.loc[queue_name, ["net_runtime_s", "threads", "delay"]]
@@ -71,31 +71,24 @@ def plot_individual_results(input_file_name:str, output_file_name:str, plot_titl
             y = "net_runtime_s",
             style = "delay",
             hue = "delay",
+            palette = palette,
             **sns_kwargs
         )
 
-        ax.set_xlabel("Threads")
-        ax.set_ylabel("Runtime (s)")
-        ax.set_title(f"{queue_name}: {plot_title}")
-
         cleaned_queue_name = str.lower(queue_name).replace(" ", "_")
 
-        plt.savefig(
-            f"{images_path}/{output_file_name}_{cleaned_queue_name}.jpg",
-            dpi = dpi
-        )
-
-        plot_cleanup()
+        file_path = f"{images_path}/{output_file_name}_{cleaned_queue_name}.jpg"
+        title = f"{queue_name}: {plot_title}"
+        save_plot(ax, file_path, title, dpi)
 
 enqueue_dequeue_title = "Pairwise Enqueue Dequeue Benchmark"
 enqueue_dequeue_file_name = "enqueue_dequeue_results"
 p_enqueue_dequeue_title = "50% Enqueue Benchmark"
 p_enqueue_dequeue_file_name = "p_enqueue_dequeue_results"
 
-
 plot_grouped_results(250, "enqueue_dequeue_results", "grouped_enqueue_dequeue", enqueue_dequeue_title)
 plot_grouped_results(250, "p_enqueue_dequeue_results", "p_grouped_enqueue_dequeue", p_enqueue_dequeue_title)
 
-plot_individual_results("enqueue_dequeue_results", "individual_enqueue_dequeue", enqueue_dequeue_title)
-plot_individual_results("p_enqueue_dequeue_results", "individual_p_enqueue_dequeue", p_enqueue_dequeue_title)
+plot_individual_results("enqueue_dequeue_results", "enqueue_dequeue", enqueue_dequeue_title)
+plot_individual_results("p_enqueue_dequeue_results", "p_enqueue_dequeue", p_enqueue_dequeue_title)
 

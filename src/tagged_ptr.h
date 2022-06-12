e696a6a615d009ac30332c7fc04502c19638f636
Luca Muscat <lucamuscat@gmail.com>
2022-06-12 17:31:15 +0200

    Add flagged tagged pointers to tagged_ptr.h

--- src/tagged_ptr.h
+++ src/tagged_ptr.h
@@ -4,16 +4,21 @@
 #include <stdint.h>
 #include "alignment_utils.h"
 
+#define TAG_WORD_MAX(tag_word_size) ((((tag_word)1) << ((tag_word_size) - 1)) | \
+ ((((tag_word)1) << ((tag_word_size) - 1)) - 1))
+
 #ifndef DWCAS // Used tagged pointers
+
 // Followed using:
 // https ://www.boost.org/doc/libs/1_78_0/boost/lockfree/detail/tagged_ptr_ptrcompression.hpp
 // (1L<<48L)-1, zeros out the 16 most significant bits.
 #define LINEAR_ADDRESS_MASK 0xffffffffffffUL
-#define TAG_INDEX 15
 #define TAG_SIZE 16
+#define TAG_INDEX (TAG_SIZE - 1)
 #define LINEAR_ADDRESS_SIZE 48
-#define UINT15_T_MASK 0x7fff
+#define UINT15_T_MASK ((1<<15) - 1)
 
+typedef uint16_t tag_word;
 typedef uintptr_t tagged_ptr_t;
 
 #define extract_ptr(ptr) ((void*)(((uintptr_t)ptr) & LINEAR_ADDRESS_MASK))
@@ -46,21 +51,38 @@ typedef uintptr_t tagged_ptr_t;
 #define equals(a, b) ((a) == (b))
 
 #else // DWCAS is defined
-
+#define TAG_SIZE 64
 typedef struct tagged_ptr_t
 {
     void* ptr;
-    uint64_t tag;
+    union
+    {
+        uint64_t tag;
+        struct
+        {
+            uint64_t flag : 1, flagged_tag : 63;
+        };
+    };
 } DWCAS_ALIGNED tagged_ptr_t;
 
-#define pack_ptr(_ptr, _count) ((tagged_ptr_t){.ptr = (_ptr), .tag = (_count)})
+typedef uint64_t tag_word;
+
+#define pack_ptr(_ptr, _tag) ((tagged_ptr_t){.ptr = (_ptr), .tag = ((uint64_t)(_tag))})
 #define extract_ptr(_ptr) (((tagged_ptr_t)(_ptr)).ptr)
 #define extract_tag(_ptr) (((tagged_ptr_t)(_ptr)).tag)
+
 #define equals(a, b) ({\
-tagged_ptr_t temp1 = (tagged_ptr_t)a; \
-tagged_ptr_t temp2 = (tagged_ptr_t)b; \
-((temp1.ptr == temp2.ptr) && (temp1.tag == temp2.tag)); \
+    tagged_ptr_t temp1 = (tagged_ptr_t)a; \
+    tagged_ptr_t temp2 = (tagged_ptr_t)b; \
+    ((temp1.ptr == temp2.ptr) && (temp1.tag == temp2.tag)); \
 })
+
+#define pack_ptr_with_flag(_ptr, _tag, _flag) ((tagged_ptr_t){ \
+    .ptr = (_ptr), \
+    .flagged_tag = ((_tag)%(TAG_WORD_MAX(TAG_SIZE-1))), \
+    .flag=((bool)(_flag))})
+#define extract_flag(_ptr) (((tagged_ptr_t)(_ptr)).flag)
+#define extract_flagged_tag(_ptr) (((tagged_ptr_t)(_ptr)).flagged_tag)
 
 #endif
 #endif

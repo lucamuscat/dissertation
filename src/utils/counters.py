46572cbf1fa68029c3d122ddd0937dee1f2fb619
Luca Muscat <luca.muscat.19@um.edu.mt>
2022-08-06 18:12:45 +0200

    Add baskets queue to `counters.py`

--- src/utils/counters.py
+++ src/utils/counters.py
@@ -54,6 +54,26 @@ MS_QUEUE_COUNTER_LABELS = {
     "d15":"Dequeue Failed to Swing Non Empty Queue (D15-D16)",
 }
 
+BASKETS_QUEUE_COUNTER_LABELS = {
+    "E05-E06" : "enqueue_count (E05-E06)",
+    "E06-E07" : "enqueue_consistent_tail_count (E06-E07)",
+    "E07-E08" : "enqueue_next_ptr_null_count (E07-E08)",
+    "E09-E10" : "enqueue_cas_tail_count (E09-E10)",
+    "E18-E19" : "enqueue_build_basket_count (E18-E19)",
+    "E22" : "enqueue_update_tail_count (E22)",
+    "E21-E22" : "enqueue_next_ptr_not_null_count (E21-E22)",
+    "D04-D05" : "dequeue_count (D04-D05)",
+    "D06-D07" : "dequeue_consistent_head_count (D06-D07)",
+    "D06-D07 (2)" : "dequeue_single_item_count (D06-D07)",
+    "D07-D08" : "dequeue_empty_count (D07-D08)",
+    "D19-D20" : "dequeue_inconsistent_head_count (D19-D20)",
+    "D21-D22" : "dequeue_consistent_iter_tail_count (D21-D22)",
+    "CAS0": "dequeue_update_tail_count",
+    "CAS1":"dequeue_freechain_update_head_count",
+    "freechain" : "dequeue_freechain_count",
+    "D28 - D29" : "dequeue_cas_failed_count (D28 - D29)"
+}
+
 ENQUEUE_DEQUEUE_FILE_NAME = "enqueue_dequeue_results.csv"
 P_ENQUEUE_DEQUEUE_FILE_NAME = "p_enqueue_dequeue_results.csv"
 TEST_RERUNS = 10
@@ -83,6 +103,18 @@ counters = [
         MS_QUEUE_NAME_DWCAS,
         "p_ms_queue_dwcas",
     ),
+    (
+        ENQUEUE_DEQUEUE_FILE_NAME,
+        BASKETS_QUEUE_COUNTER_LABELS,
+        "Baskets Queue using Tagged Pointers",
+        "baskets_tagged_ptr"
+    ),
+    (
+        ENQUEUE_DEQUEUE_FILE_NAME,
+        BASKETS_QUEUE_COUNTER_LABELS,
+        "Baskets Queue using DWCAS",
+        "baskets_dwcas"
+    )
 ]
 
 
@@ -94,8 +126,6 @@ def save_counters_to_html(input_file_name: str, labels, queue_name, output_file_
     return df
 
 if __name__ == "__main__":
-
-
     COUNTERS_DIR = "src/utils/counters"
 
     os.makedirs(COUNTERS_DIR, exist_ok=True)
@@ -107,4 +137,3 @@ if __name__ == "__main__":
 
     for counter in counters:
         counters_df.append(save_counters_to_html(*counter))
-
